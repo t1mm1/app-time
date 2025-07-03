f3883088bf48d617035b78e78f64e41776b4cf46
Pavel Kasianov <p.kasianov@gmail.com>
2025-07-03 22:31:36 +0300

    Added variables into docker-compose.yml file (from .env, .env wasnt added into repository, use sample)

--- api/src/utils/mysqlPool.mjs
+++ api/src/utils/mysqlPool.mjs
@@ -2,11 +2,11 @@ import mysql from 'mysql2'
 
 const pool = mysql.createPool({
   connectionLimit: 100,
-  host: 'mysql',
-  port: '3306',
-  user: 'root',
-  password: 'password',
-  database: 'time_db',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_NAME,
 })
 
 const CREATE_TIMES_TABLE_SQL = `CREATE TABLE IF NOT EXISTS times (
