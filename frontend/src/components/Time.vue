561599e7262755d5fa9c7ffa52546dd46838685e
Pavel Kasianov <p.kasianov@gmail.com>
2025-07-03 21:06:53 +0300

    Added volumes for app files, added correct name for services inside docker-compose

--- frontend/src/components/Time.vue
+++ frontend/src/components/Time.vue
@@ -1,6 +1,6 @@
 <script>
-import moment from 'moment'
-import { startInterval, saveTime, deleteTime } from '../utils/time'
+import moment from 'moment';
+import { startInterval, saveTime, deleteTime } from '../utils/time';
 
 export default {
   props: {
@@ -10,7 +10,7 @@ export default {
     return {
       currentTime: moment().format('HH:mm:ss'),
       savedTimes: [],
-    }
+    };
   },
   methods: {
     startInterval,
@@ -18,19 +18,19 @@ export default {
     deleteTime,
   },
   created: async function () {
-    this.startInterval()
-    const res = await fetch('http://localhost:5555/times')
-    const json = await res.json()
-    if (json.length) this.savedTimes = json
+    this.startInterval();
+    const res = await fetch('http://localhost:5555/times');
+    const json = await res.json();
+    if (json.length) this.savedTimes = json;
   },
-}
+};
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ currentTime }}</h1>
     <h3>Нажмите кнопку для сохранения в базе данных</h3>
-    <button @click="saveTime">Сохранить время</button>
+    <button @click="saveTime">Сохранить текущее время</button>
     <h3 v-if="savedTimes.length && showSavedTimes">
       Ранее сохраненные времена:
     </h3>
